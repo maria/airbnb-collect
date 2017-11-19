9f034aa814fb5415dfefe981977f4f7a4d1fc00e
Maria Nita <maria.nita.dn@gmail.com>
2017-11-19 17:09:18 +0200

    Create db and import it

--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 set -eo pipefail
 
+exec psql --host db --username postgres <<-EOSQL
+    CREATE USER airbnb;
+    CREATE DATABASE airbnb;
+    GRANT ALL PRIVILEGES ON DATABASE airbnb TO airbnb;
+EOSQL
 exec psql --host db --username airbnb airbnb < airbnb-data-collection-0.1/postgresql/schema_current.sql "$@"
